f20c5a714cd57b576adab8d25c6f257fcae9d16e
Spencer Olson <solson@instructure.com>
2024-09-18 23:55:54 -0500

    temporarily comment out fingerprinter

--- lib/pg_query/fingerprinter.ex
+++ lib/pg_query/fingerprinter.ex
@@ -1,37 +1,37 @@
 defmodule PgQuery.Fingerprinter do
-  @on_load :init
+  # @on_load :init
 
-  defmodule Error do
-    defexception [:message]
-  end
+  # defmodule Error do
+  #   defexception [:message]
+  # end
 
-  def init do
-    :ok = load_nif()
-  end
+  # def init do
+  #   :ok = load_nif()
+  # end
 
-  defp load_nif do
-    :pg_query_ex
-    |> Application.app_dir("priv/libpg_query_ex")
-    |> String.to_charlist()
-    |> :erlang.load_nif(0)
-  end
+  # defp load_nif do
+  #   :pg_query_ex
+  #   |> Application.app_dir("priv/libpg_query_ex")
+  #   |> String.to_charlist()
+  #   |> :erlang.load_nif(0)
+  # end
 
-  def fingerprint(query) when is_binary(query) do
-    with {:ok, proto} <- fingerprint_query(query) do
-      # Protox.decode(proto, PgQuery.ParseResult)
-      proto
-    end
-  end
-
-  # def parse!(query) when is_binary(query) do
-  #   case parse_query(query) do
-  #     {:ok, proto} ->
-  #       Protox.decode!(proto, PgQuery.ParseResult)
-
-  #     {:error, reason} ->
-  #       raise Error, message: reason
+  # def fingerprint(query) when is_binary(query) do
+  #   with {:ok, proto} <- fingerprint_query(query) do
+  #     # Protox.decode(proto, PgQuery.ParseResult)
+  #     proto
   #   end
   # end
 
-  def fingerprint_query(_query), do: :erlang.nif_error(:nif_not_loaded)
+  # # def parse!(query) when is_binary(query) do
+  # #   case parse_query(query) do
+  # #     {:ok, proto} ->
+  # #       Protox.decode!(proto, PgQuery.ParseResult)
+
+  # #     {:error, reason} ->
+  # #       raise Error, message: reason
+  # #   end
+  # # end
+
+  # def fingerprint_query(_query), do: :erlang.nif_error(:nif_not_loaded)
 end
